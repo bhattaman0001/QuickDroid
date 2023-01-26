3c31bddc3a0f540978b802006528b1601949e123
Aman Bhatt <aman.bhatt@appyhigh.com>
2023-01-26 22:59:48 +0530

    initial commit

---
+++ app/src/main/java/com/example/moviemagnet/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.moviemagnet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val spinner: Spinner = findViewById(R.id.spinner)
+        val adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.spinner_options,
+            android.R.layout.simple_spinner_item
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                // Handle the selected item here
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // Handle the case when nothing is selected
+            }
+        }
+
+    }
+
+}
