4e4f9435674c343b19330af45128200e52d5954f
Aman Bhatt <aman.bhatt@appyhigh.com>
2023-06-03 00:49:13 +0530

    Added spinner and made home page UI

---
+++ app/src/main/java/com/example/moviemagnet/Constants/constants.kt
@@ -0,0 +1,6 @@
+package com.example.moviemagnet.Constants
+
+object constants {
+    val BASE_URL = "https://filepursuit.p.rapidapi.com/"
+    /*https://filepursuit.p.rapidapi.com/?q=The%20Last:%20Naruto%20the%20Movie&type=video*/
+}
--- app/src/main/java/com/example/moviemagnet/MainActivity.kt
+++ app/src/main/java/com/example/moviemagnet/MainActivity.kt
@@ -1,41 +1,38 @@
 package com.example.moviemagnet
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Spinner
+import android.os.*
+import android.util.*
+import android.view.*
+import android.widget.*
+import androidx.appcompat.app.*
+import com.example.moviemagnet.databinding.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    var type_of_file: Array<String> = emptyArray()
+    var type_of_file_selected: String = ""
+    var query_name: String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val spinner: Spinner = findViewById(R.id.spinner)
-        val adapter = ArrayAdapter.createFromResource(
-            this,
-            R.array.spinner_options,
-            android.R.layout.simple_spinner_item
-        )
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        spinner.adapter = adapter
-
-        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
-            ) {
-                // Handle the selected item here
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view: View = binding.root
+        setContentView(view)
+        type_of_file = resources.getStringArray(R.array.spinner_options)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, type_of_file)
+        binding.typeOfFile.adapter = adapter
+        binding.typeOfFile.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                if (position == 0) type_of_file_selected = ""
+                if (position >= 1) type_of_file_selected = type_of_file[position]
+                Log.d("is_this_ok", "the selected type is --> $type_of_file_selected")
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
-                // Handle the case when nothing is selected
+                // todo: do something when nothing is selected
             }
         }
-
+        if (binding.queryName != null) query_name = binding.queryName.text.toString()
     }
 
 }
